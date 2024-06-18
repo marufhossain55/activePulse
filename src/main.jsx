3d7ffaf5ccbdf88c765fb262fd00712c5ca595a7
marufhossain55 <engrmaruf3@gmail.com>
2024-06-18 19:41:35 +0600

    provider running

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/Routes.jsx';
+import { AuthContext } from './provider/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthContext>
+      <RouterProvider router={router} />
+    </AuthContext>
   </React.StrictMode>
 );
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,4 +1,12 @@
-const AuthProvider = () => {
-  return <div>AuthProvider</div>;
+import { Children, createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+  const authInfo = { user, loading };
+  return (
+    <AuthContext.Provider value={authInfo}>{Children}</AuthContext.Provider>
+  );
 };
 export default AuthProvider;
