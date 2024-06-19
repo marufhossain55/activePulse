b041417b80d2d7f599e20d60d2e536e6796e85e1
marufhossain55 <engrmaruf3@gmail.com>
2024-06-19 10:53:57 +0600

    firebase api fixed

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,9 +1,23 @@
-import { Children, createContext, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { Children, createContext, useEffect, useState } from 'react';
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState();
   const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+  //     setUser(currentUser);
+  //     console.log('current user', currentUser);
+  //   });
+  //   return () => {
+  //     return unsubscribe();
+  //   };
+  // }, []);
+
   const authInfo = { user, loading };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
