07619d7d960e91d7c8ab04270b05586e3edfc0d6
marufhossain55 <engrmaruf3@gmail.com>
2024-06-24 01:13:38 +0600

    manage slote done

--- src/pages/dashboard/ManageSlots.jsx
+++ src/pages/dashboard/ManageSlots.jsx
@@ -1,43 +1,366 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import Swal from 'sweetalert2';
+// import useAxiosSecure from '../../hooks/useAxiosSecure';
+// const ManageSlots = () => {
+//   const axiosSecure = useAxiosSecure();
+//   const [slots, setSlots] = useState([]);
+//   useEffect(() => {
+//     const fetchSlots = async () => {
+//       try {
+//         const res = await axiosSecure.get('/getAllSlots', {
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem('access-token')}`,
+//           },
+//         });
+//         setSlots(res.data);
+//       } catch (error) {
+//         console.error('Error fetching slots:', error);
+//       }
+//     };
+
+//     fetchSlots();
+//   }, [axiosSecure]);
+
+//   const handleDeleteSlot = async (slotId) => {
+//     const confirmed = await Swal.fire({
+//       title: 'Are you sure?',
+//       text: 'You will not be able to recover this slot!',
+//       icon: 'warning',
+//       showCancelButton: true,
+//       confirmButtonColor: '#3085d6',
+//       cancelButtonColor: '#d33',
+//       confirmButtonText: 'Yes, delete it!',
+//     });
+
+//     if (confirmed.isConfirmed) {
+//       try {
+//         const res = await axiosSecure.delete(`/deleteSlot/${slotId}`, {
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem('access-token')}`,
+//           },
+//         });
+//         if (res.data.deletedCount === 1) {
+//           Swal.fire({
+//             position: 'top-end',
+//             icon: 'success',
+//             title: 'Slot deleted successfully',
+//             showConfirmButton: false,
+//             timer: 1500,
+//           });
+//           setSlots(slots.filter((slot) => slot._id !== slotId));
+//         }
+//       } catch (error) {
+//         console.error('Error deleting slot:', error);
+//       }
+//     }
+//   };
+//   return (
+//     <div className="container mx-auto p-6">
+//       <h1 className="text-center text-3xl font-bold mt-20">Manage Slots</h1>
+//       <div className="mt-8">
+//         <table className="min-w-full bg-white border-gray-200 shadow-md rounded-lg overflow-hidden">
+//           <thead className="bg-gray-100 border-b">
+//             <tr>
+//               <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+//                 Slot Name
+//               </th>
+//               <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+//                 Slot Time
+//               </th>
+//               <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+//                 Days
+//               </th>
+//               <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+//                 Classes
+//               </th>
+//               <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+//                 Actions
+//               </th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {slots?.map((slot) => (
+//               <tr key={slot._id} className="hover:bg-gray-50 border-b">
+//                 <td className="py-2 px-4">{slot?.slotName}</td>
+//                 <td className="py-2 px-4">{slot?.slotTime}</td>
+//                 <td className="py-2 px-4">{slot?.days?.join(', ')}</td>
+//                 <td className="py-2 px-4">
+//                   {slot?.classes
+//                     ?.map((classItem) => classItem?.name)
+//                     .join(', ')}
+//                 </td>
+//                 <td className="py-2 px-4">
+//                   <button
+//                     className="text-red-500 hover:text-red-700 focus:outline-none"
+//                     onClick={() => handleDeleteSlot(slot._id)}
+//                   >
+//                     <svg
+//                       className="w-5 h-5"
+//                       fill="none"
+//                       strokeLinecap="round"
+//                       strokeLinejoin="round"
+//                       strokeWidth="2"
+//                       viewBox="0 0 24 24"
+//                       stroke="currentColor"
+//                     >
+//                       <path d="M6 18L18 6M6 6l12 12"></path>
+//                     </svg>
+//                   </button>
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// };
+// export default ManageSlots;
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import Swal from 'sweetalert2';
+// import useAxiosSecure from '../../hooks/useAxiosSecure';
+
+// const ManageSlots = () => {
+//   const axiosSecure = useAxiosSecure();
+//   const [slots, setSlots] = useState([]);
+
+//   useEffect(() => {
+//     const fetchSlots = async () => {
+//       try {
+//         const res = await axiosSecure.get('/getAllSlots', {
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem('access-token')}`,
+//           },
+//         });
+//         setSlots(res.data);
+//       } catch (error) {
+//         console.error('Error fetching slots:', error);
+//       }
+//     };
+
+//     fetchSlots();
+//   }, [axiosSecure]);
+
+//   const handleDeleteSlot = async (slot) => {
+//     console.log(slot);
+//     const confirmed = await Swal.fire({
+//       title: 'Are you sure?',
+//       text: 'You will not be able to recover this slot!',
+//       icon: 'warning',
+//       showCancelButton: true,
+//       confirmButtonColor: '#3085d6',
+//       cancelButtonColor: '#d33',
+//       confirmButtonText: 'Yes, delete it!',
+//     });
+
+//     if (confirmed.isConfirmed) {
+//       try {
+//         const res = await axiosSecure.delete(`/deleteSlot/${slot._id}`, {
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem('access-token')}`,
+//           },
+//         });
+//         if (res.data.deletedCount === 1) {
+//           Swal.fire({
+//             position: 'top-end',
+//             icon: 'success',
+//             title: 'Slot deleted successfully',
+//             showConfirmButton: false,
+//             timer: 1500,
+//           });
+//           setSlots(slots.filter((slot) => slot._id !== slotId));
+//         }
+//       } catch (error) {
+//         console.error('Error deleting slot:', error);
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className="container mx-auto p-6">
+//       <h1 className="text-center text-3xl font-bold mt-20">Manage Slots</h1>
+//       <div className="mt-8">
+//         <table className="min-w-full bg-white border-gray-200 shadow-md rounded-lg overflow-hidden">
+//           <thead className="bg-gray-100 border-b">
+//             <tr>
+//               <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+//                 Slot Name
+//               </th>
+//               <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+//                 Slot Time
+//               </th>
+//               <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+//                 Days
+//               </th>
+//               <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+//                 Classes
+//               </th>
+//               <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+//                 Actions
+//               </th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {slots?.map((slot) => (
+//               <tr key={slot?._id} className="hover:bg-gray-50 border-b">
+//                 <td className="py-2 px-4">{slot?.slotName}</td>
+//                 <td className="py-2 px-4">{slot?.slotTime}</td>
+//                 <td className="py-2 px-4">{slot?.days?.join(', ')}</td>
+//                 <td className="py-2 px-4">
+//                   {slot?.classes?.map((classItem) => classItem).join(', ')}
+//                 </td>
+//                 <td className="py-2 px-4">
+//                   <button
+//                     className="text-red-500 hover:text-red-700 focus:outline-none"
+//                     onClick={() => handleDeleteSlot(slot)}
+//                   >
+//                     <svg
+//                       className="w-5 h-5"
+//                       fill="none"
+//                       strokeLinecap="round"
+//                       strokeLinejoin="round"
+//                       strokeWidth="2"
+//                       viewBox="0 0 24 24"
+//                       stroke="currentColor"
+//                     >
+//                       <path d="M6 18L18 6M6 6l12 12"></path>
+//                     </svg>
+//                   </button>
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ManageSlots;
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+
 const ManageSlots = () => {
-  return  const { user } = useAuth();
-  console.log(user);
+  const axiosSecure = useAxiosSecure();
+  const [slots, setSlots] = useState([]);
+
+  useEffect(() => {
+    const fetchSlots = async () => {
+      try {
+        const res = await axiosSecure.get('/getAllSlots', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('access-token')}`,
+          },
+        });
+        setSlots(res.data);
+      } catch (error) {
+        console.error('Error fetching slots:', error);
+      }
+    };
+
+    fetchSlots();
+  }, [axiosSecure]);
+
+  const handleDeleteSlot = async (slot) => {
+    const confirmed = await Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this slot!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    });
+
+    if (confirmed.isConfirmed) {
+      try {
+        const res = await axiosSecure.delete(`/deleteSlot/${slot._id}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('access-token')}`,
+          },
+        });
+        if (res.data.deletedCount === 1) {
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Slot deleted successfully',
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          // Update slots state after deletion
+          const updatedSlots = slots.filter((s) => s._id !== slot._id);
+          setSlots(updatedSlots);
+        }
+      } catch (error) {
+        console.error('Error deleting slot:', error);
+      }
+    }
+  };
+
   return (
-    <div class="max-w-md p-8 sm:flex sm:space-x-6 bg-gray-900 dark:bg-gray-50 text-gray-100 dark:text-gray-800 mx-auto mt-16">
-      <div class="flex-shrink-0 w-full mb-6 h-44 sm:h-32 sm:w-32 sm:mb-0">
-        <img
-          src={user?.photoURL}
-          alt={user?.photoURL}
-          class="object-cover object-center w-full h-full rounded bg-gray-500 dark:bg-gray-500"
-        />
-      </div>
-      <div class="flex flex-col space-y-4">
-        <div>
-          <h2 class="text-2xl font-semibold">{user?.displayName}</h2>
-          <span class="text-sm text-gray-400 dark:text-gray-600">Member</span>
-        </div>
-        <div class="space-y-1">
-          <span class="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              aria-label="Email address"
-              class="w-4 h-4"
-            >
-              <path
-                fill="currentColor"
-                d="M274.6,25.623a32.006,32.006,0,0,0-37.2,0L16,183.766V496H496V183.766ZM464,402.693,339.97,322.96,464,226.492ZM256,51.662,454.429,193.4,311.434,304.615,256,268.979l-55.434,35.636L57.571,193.4ZM48,226.492,172.03,322.96,48,402.693ZM464,464H48V440.735L256,307.021,464,440.735Z"
-              ></path>
-            </svg>
-            <span class="text-gray-400 dark:text-gray-600">{user?.email}</span>
-          </span>
-          <span class="flex items-center space-x-2">
-            <span class="text-gray-400 dark:text-gray-600">{`last login: ${user.metadata.lastSignInTime}`}</span>
-          </span>
-        </div>
+    <div className="container mx-auto p-6">
+      <h1 className="text-center text-3xl font-bold mt-20">Manage Slots</h1>
+      <div className="mt-8">
+        <table className="min-w-full bg-white border-gray-200 shadow-md rounded-lg overflow-hidden">
+          <thead className="bg-gray-100 border-b">
+            <tr>
+              <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+                Slot Name
+              </th>
+              <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+                Slot Time
+              </th>
+              <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+                Days
+              </th>
+              <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+                Classes
+              </th>
+              <th className="py-2 px-4 text-left uppercase font-semibold text-sm">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {slots?.map((slot) => (
+              <tr key={slot?._id} className="hover:bg-gray-50 border-b">
+                <td className="py-2 px-4">{slot?.slotName}</td>
+                <td className="py-2 px-4">{slot?.slotTime}</td>
+                <td className="py-2 px-4">{slot?.days?.join(', ')}</td>
+                <td className="py-2 px-4">
+                  {slot?.classes?.map((classItem) => classItem).join(', ')}
+                </td>
+                <td className="py-2 px-4">
+                  <button
+                    className="text-red-500 hover:text-red-700 focus:outline-none"
+                    onClick={() => handleDeleteSlot(slot)}
+                  >
+                    <svg
+                      className="w-5 h-5"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 };
-export default Profile;
 
 export default ManageSlots;
