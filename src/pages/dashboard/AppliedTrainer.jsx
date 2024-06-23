51b707f3601909dfe85d974224663e4e5676b400
marufhossain55 <engrmaruf3@gmail.com>
2024-06-23 18:35:13 +0600

    applied trainer modified

--- src/pages/dashboard/AppliedTrainer.jsx
+++ src/pages/dashboard/AppliedTrainer.jsx
@@ -1,76 +1,8 @@
-// import { useQuery } from '@tanstack/react-query';
-// import useAxiosSecure from '../../hooks/useAxiosSecure';
-// import { FaEye } from 'react-icons/fa';
-
-// const AppliedTrainer = () => {
-//   const axiosSecure = useAxiosSecure();
-//   const { data: applications } = useQuery({
-//     queryKey: ['application'],
-//     queryFn: async () => {
-//       const res = await axiosSecure.get('/applyForTrainer', {
-//         headers: {
-//           Authorization: `Bearer ${localStorage.getItem('access-token')}`,
-//         },
-//       });
-//       return res.data;
-//     },
-//   });
-//   console.log(applications);
-//   return (
-//     <div>
-//       <h1 className="text-center text-3xl font-bold mt-20">Applied Trainer</h1>
-//       <div className="container mx-auto p-6">
-//         <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
-//           <thead className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
-//             <tr>
-//               <th className="py-3 px-6 text-left">SN</th>
-//               <th className="py-3 px-6 text-left">Name</th>
-//               <th className="py-3 px-6 text-left">Email</th>
-//               <th className="py-3 px-6 text-left">Action</th>
-//             </tr>
-//           </thead>
-//           <tbody className="text-gray-600 text-sm font-light">
-//             {applications?.map((application, idx) => (
-//               <tr
-//                 key={application._id}
-//                 className="border-b border-gray-200 hover:bg-gray-100"
-//               >
-//                 <td className="py-3 px-6 text-left whitespace-nowrap">
-//                   <div className="flex items-center">
-//                     <span className="font-medium">{idx + 1}</span>
-//                   </div>
-//                 </td>
-//                 <td className="py-3 px-6 text-left">
-//                   <div className="flex items-center">
-//                     <span>{application.name}</span>
-//                   </div>
-//                 </td>
-//                 <td className="py-3 px-6 text-left">
-//                   <div className="flex items-center">
-//                     <span>{application.email}</span>
-//                   </div>
-//                 </td>
-//                 <td className="py-3 px-6 text-left">
-//                   <div className="flex items-center ">
-//                     <span className="border border-emerald-600 bg-emerald-400 px-4 py-2 rounded cursor-pointer">
-//                       <FaEye size={24} />
-//                     </span>
-//                   </div>
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     </div>
-//   );
-// };
-// export default AppliedTrainer;
-
 import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../../hooks/useAxiosSecure';
 import { FaEye, FaTimes } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const AppliedTrainer = () => {
   const axiosSecure = useAxiosSecure();
@@ -99,9 +31,22 @@ const AppliedTrainer = () => {
     setSelectedApplication(null);
   };
 
-  const confirmTrainer = async (id) => {
+  const confirmTrainer = async (selectedApplication) => {
+    console.log(selectedApplication.email);
     try {
-      await axiosSecure.post(`/confirm-trainer/${id}`);
+      const res = await axiosSecure.post(
+        `/confirmTrainer/${selectedApplication.email}`
+      );
+      console.log(res.data);
+      if (res.data.acknowledged) {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'trainer accepted',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
       closeModal();
       // Optionally, refetch applications or update the state
     } catch (error) {
@@ -185,7 +130,7 @@ const AppliedTrainer = () => {
             {/* Add other application details here if needed */}
             <div className="flex justify-end mt-4">
               <button
-                onClick={() => confirmTrainer(selectedApplication._id)}
+                onClick={() => confirmTrainer(selectedApplication)}
                 className="bg-green-500 text-white px-4 py-2 rounded mr-2 hover:bg-green-600"
               >
                 Confirm
